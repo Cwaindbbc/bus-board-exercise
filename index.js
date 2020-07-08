e1cc2b9ae20f1d2556dbe493a78df7ef38a5d5e2
Catherine Waind <catherine.waind@bbc.co.uk>
2020-07-08 16:46:07 +0100

    Displaying first five buses at a stop

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-This is a test 
+
+const request = require('request');
+request('https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption?app_id=483499fa&app_key=4ae112a69209ec408b173e4e9db18388', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', coresponse && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+  const returnedBody = response.body
+    function firstFive(returnedBody){
+    const fiveback = returnedBody[0,4];
+    return fiveback;
+    console.log(fiveback);
+}
+});
+firstFive()
+// and then print it out the next 5 buses at that stop. 
